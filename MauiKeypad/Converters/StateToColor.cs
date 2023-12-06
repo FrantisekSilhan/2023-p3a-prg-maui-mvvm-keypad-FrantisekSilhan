bf20e3e9c30c0cc92755455b79bbe711206ef58d
František Šilhán <frantiseksilhann@gmail.com>
2023-12-06 09:02:40 +0000

    colors

---
+++ MauiKeypad/Converters/StateToColor.cs
@@ -0,0 +1,30 @@
+﻿using MauiKeypad.Models;
+using System.Globalization;
+
+namespace MauiKeypad.Converters
+{
+    internal class StateToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is EntryState)
+            {
+                switch (value)
+                {
+                    case (EntryState.InProgress):
+                        return Color.FromRgb(10, 10, 10);
+                    case (EntryState.Success):
+                        return Color.FromRgb(10, 255, 10);
+                    case (EntryState.Denied):
+                        return Color.FromRgb(255, 10, 10);
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
